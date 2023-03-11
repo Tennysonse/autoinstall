2af16a616e7c1bb3cea1c6852a608811403faf52
wanguqan <125903007+Tennysonse@users.noreply.github.com>
2023-03-11 13:36:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,23 +8,16 @@ cd /root/autoinstall
 unzip tftp.zip
 systemctl stop tftpd-hpa
 systemctl stop systemd-resolved
-apt-get -y install dnsmasq apache2 whois
-pxe_server_ip='10.0.0.3'
-iso_name='ubuntu-20.04.2-live-server-amd64.iso'
-sed -i s/10.0.0.3/$pxe_server_ip/g /root/autoinstall/tftpboot/grub/grub.cfg
-sed -i s/ubuntu-20.04.2-live-server-amd64.iso/$iso_name/g /root/autoinstall/tftpboot/grub/grub.cfg
-sed -i s/10.0.0.3/$pxe_server_ip/g /root/autoinstall/tftpboot/pxelinux.cfg/default
-sed -i s/ubuntu-20.04.2-live-server-amd64.iso/$iso_name/g /root/autoinstall/tftpboot/pxelinux.cfg/default
 real_name='ub'
 #passwd 加密方法
 user_passwd=`mkpasswd -m sha-512 '123456'`
 
 cat > /etc/apache2/conf-available/tftp.conf <<EOF
-<Directory /var/lib/tftpboot>
+<Directory /var/autoinstall/tftpboot>
         Options +FollowSymLinks +Indexes
         Require all granted
 </Directory>
-Alias /tftp /root/autoinstall/tftpboot
+Alias /tftp /var/autoinstall/tftpboot
 EOF
 
 cat >> /etc/dnsmasq.conf <<EOF
@@ -43,23 +36,11 @@ dhcp-range=10.0.0.156,10.0.0.200,255.255.255.0,12h
 
 
 
-#-- 设置tftp根路径
 enable-tftp
 tftp-root=/root/autoinstall/tftpboot
-enable-tftp
-#--设置引导程序相对tftp根目录的路径
 dhcp-boot=/pxelinux.0,pxeserver,10.0.0.150
-#--检测架构并发送正确的引导加载程序文件 
 dhcp-match=set:efi-x86_64,option:client-arch,7 
 dhcp-boot=tag:efi-x86_64,grub/pxelinux.0
-
-a2enconf tftp
 EOF
-mkdir -p /mnt
-cp ubuntu-20.04.2-live-server-amd64.iso /root/autoinstall/tftpboot
-mount /root/autoinstall/tftpboot/ubuntu-20.04.2-live-server-amd64.iso /mnt/
-cp /mnt/casper/vmlinuz /root/autoinstall/tftpboot
-cp /mnt/casper/initrd /root/autoinstall/tftpboot
-umoun -l /mnt
-
+a2enconf tftp
 systemctl restart apache2 dnsmasq
